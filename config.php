9bab33427312b68e933e5fb72fa4a2b8495b1836
kirilkorukov <64249309+kirilkorukov@users.noreply.github.com>
2021-05-15 18:23:13 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = new mysqli("localhost", "root","","dream-world");
+  $conn = new mysqli("eu-cdbr-west-01.cleardb.com", "b37fe0931e383b","8c250a9f","heroku_532a59e461ef8d5");
 
   if(!$conn) echo "error";
 ?>
